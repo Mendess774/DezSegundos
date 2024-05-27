952111d457aef9e764efafa5264894e02e7ab00d
Felipe Mendes <you@example.com>
2024-05-26 22:31:57 -0300

    Aprenda a contar até 10

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿﻿using System.Diagnostics.Contracts;
+
+Console.Write("Dez Segundos");
+Thread.Sleep(1000);
+
+Console.ForegroundColor = ConsoleColor.Blue;
+
+Console.Write(" 0");
+Thread.Sleep(1000);
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+
+Console.Write(" 1");
+Thread.Sleep(1000);
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+Console.Write(" 2");
+
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+Thread.Sleep(1000);
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+Console.Write(" 3");
+Thread.Sleep(1000);
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+Console.Write(" 4");
+Thread.Sleep(1000);
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+
+Console.Write(" 5");
+Thread.Sleep(1000);
+
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+Console.Write(" 6");
+Thread.Sleep(1000);
+
+Console.ForegroundColor = ConsoleColor.Green;
+
+Console.Write(" 7");
+Thread.Sleep(1000);
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+
+Console.Write(" 8");
+Thread.Sleep(1000);
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+Console.Write(" 9");
+Thread.Sleep(1000);
+
+Console.ForegroundColor = ConsoleColor.Red;
+
+Console.Write(" 10");
+Thread.Sleep(1000);
